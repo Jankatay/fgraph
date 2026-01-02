d5a2f737132235605d677e062664e2dfe82e3102
Jankatay <barjanay@proton.me>
2026-01-02 16:21:29 -0600

    started working on the actual graphing portion

--- src/fgraph.c
+++ src/fgraph.c
@@ -9,9 +9,50 @@ struct Map {
 };
 
 
-// starting from inside a function body, list all the functions in list of "usermade" ones.
+// starting from inside a function body, list all the function calls in list of "usermade" ones.
 // sets status to 0 on error and 1 on success, nullable.
 // -------------------------------------------------------------------------------------------
+struct Vec fcalls(FILE* body, struct Vec usermade, int* status) {
+  // init 
+  struct Vec dependencies = {};
+  int statusDump = 0;
+  if(!status) status = &statusDump;
+  int curlCount = 1;
+
+  // sanitize
+  if(!body) return dependencies;
+  if(!usermade.cap) {
+    *status = 1;
+    return dependencies;
+  }
+
+  // identifier info
+  char fname[BUFSIZE];
+  int flen = 0;
+  *status = 1;
+
+  // for each token
+  enum Token left, right = TOK_ERR;
+  while(curlCount) {
+    right = ftoken(body, fname, &flen);
+    // error doesn't mean the end, continue parsing regardless.
+    if(right & (TOK_ERR | TOK_EOF)) *status = 0; 
+
+    // skip comments
+    if(tokskip(body, right)) continue;
+
+    // account for curly brackets
+    if(right == OP_CURL) curlCount++;
+    if(right == CL_CURL) curlCount--;
+
+    // check for function calls
+    if((left == TOK_IDENTIFIER) && (right == OP_PAREN)) vset(&dependencies, dependencies.cap, fname, flen);
+    right = left;
+  }
+  
+  // body is closed
+  return dependencies;
+}
 
 
 // executable form
